7fb927f1dc8305af0a7c1a73ee4646d21d6abdaf
mohd jami <your_email@example.com>
2024-06-07 09:58:34 +0530

    Refactoring

--- app.ts
+++ app.ts
@@ -1,1 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
+import { sendEmail } from "./email";
+import * as bodyParser from "body-parser";
+import { Request, Response } from "express";
+import email from "./email";
+dotenv.config();
+
+const app = express();
+const port = 8000;
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send(process.env.JAMI);
+});
+
+app.post("/send-mail", email);
+
+export default app;
